dd04160aac19b9985b55f9436e3e7f7d721e28f6
CJ Melegrito <cj@mlgrto.com>
2016-07-30 15:52:03 +1000

    Re-wrote login

---
+++ CJ/app.py
@@ -0,0 +1,35 @@
+from flask import Flask, render_template, request
+from usernames import faux_usernames
+
+app = Flask(__name__)
+
+# displays the home page
+@app.route('/')
+def home():
+	return render_template('home.html')
+
+# checks for username availability
+@app.route('/login/', methods=['POST'])
+def login():
+	username = request.form['username']
+	if username in faux_usernames:
+		return render_template('home.html', username_exists=True, username=username)
+	else:
+		return render_template('home.html', username_exists=False, username=username)
+
+# renders a user's page
+@app.route('/user/<username>')
+def user(username):
+	if username in faux_usernames:
+		return render_template('user.html', username=username)
+	else:
+		return username + " does not exist!"
+
+# creates a new user's page
+@app.route('/new/<username>')
+def new_user(username):
+	faux_usernames.append(username)
+	return render_template('user.html', username=username)
+
+if __name__ == '__main__':
+	app.run(debug=True)
---
+++ CJ/usernames.py
@@ -0,0 +1,1 @@
+faux_usernames = ['alex']
