a0ea8a6a14cb496211943d52d5664893c0560a5f
CJ Melegrito <cj@mlgrto.com>
2016-07-31 01:50:08 +1000

    Remove comment

--- CJ/app.py
+++ CJ/app.py
@@ -13,8 +13,6 @@ db = SQLAlchemy(app)
 # SQL Database
 # ---------------------------------
 
-################ DATABASE SETUP ########################
-
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80), unique=True)
