df057839cd78196f1a1e120f8d0b45525b819415
CJ Melegrito <cj@mlgrto.com>
2016-07-30 13:07:54 +1000

    Add /check_username/ post request form

--- CJ/app.py
+++ CJ/app.py
@@ -1,10 +1,18 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
 	return render_template('home.html')
+
+@app.route('/check_username/', methods=['POST'])
+def check_username():
+	username = request.form['username']
+	if username != 'bob':
+		return username + ' is free!'
+	else:
+		return 'nope!'
 
 if __name__ == '__main__':
 	app.run(debug=True)
