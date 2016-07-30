f5e2d695195af57a01746b51366bf7270aef9ccc
CJ Melegrito <cj@mlgrto.com>
2016-07-31 00:43:01 +1000

    Cleaning up codebase

--- CJ/app.py
+++ CJ/app.py
@@ -1,92 +1,133 @@
+# ---------------------------------
+# Import Modules
+# ---------------------------------
 from flask import Flask, render_template, request, redirect
+from flask_sqlalchemy import SQLAlchemy
 
-from models import *
-
-from datetime import datetime
-from base64 import b64encode
-
+# setup Flask
 app = Flask(__name__)
 
-# displays the home page
+# ---------------------------------
+# SQL Database
+# ---------------------------------
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test4.db'
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), unique=True)
+    calendar = db.Column(db.String)
+    groups = db.relationship('Group', backref='user', lazy='dynamic')
+
+    def __init__(self, name, calendar = ""):
+        self.name = name
+        self.calendar = calendar
+
+    def __repr__(self):
+        return self.name
+
+class Group(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	groupCode = db.Column(db.Integer, unique=False)
+	person_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+	def __init__(self, groupCode, person_id):
+		self.groupCode = groupCode
+		self.person_id = person_id
+
+	def __repr__(self):
+		return self.groupCode
+
+# ---------------------------------
+# Home Page
+# ---------------------------------
+
+# render homepage
 @app.route('/')
 def home():
 	return render_template('home.html')
 
-# checks for username availability
+# check username availability
 @app.route('/', methods=['POST'])
 def check_username():
+	# get data from form
 	username = request.form['username']
-	if username in faux_usernames:
-		return render_template('home.html', username_exists=True, username=username)
-	else:
-		return render_template('home.html', username_exists=False, username=username)
+	username_exists = None
 
-# renders a user's page
+	# query the database for a username
+	db_user = User.query.filter_by(name=username).first()
+
+	# set username_exists flag
+	if db_user:
+		username_exists = True
+	else:
+		username_exists = False
+
+	# render homepage
+	return render_template('home.html', username_exists=username_exists, username=username)
+
+# ---------------------------------
+# User Page
+# ---------------------------------
+
+# render user page
 @app.route('/user/<username>')
 def user(username):
-	if username in faux_usernames:
-		events = faux_users[username]
+	# query the database for a username
+	db_user = User.query.filter_by(name=username).first()
+
+
+	if username in users_database:
+		events = users_database[username]
 		return render_template('user.html', username=username, events=events)
 	else:
 		return render_template('404.html', value=username)
 
-# creates a new user's page
+# create a new user page
 @app.route('/new_user/<username>')
 def new_user(username):
-	faux_usernames.append(username)
-	faux_users[username] = {}
+	# add the user to the database
+	users_database[username] = []
+
+	# redirect to newly created user page
 	url = '/user/' + username
 	return redirect(url)
 
-# creates a new group
-@app.route('/new_group/')
-def new_group():
-	group_code = generate_code()
-	faux_groups[group_code] = []
-	url = '/group/' + group_code
-	return redirect(url)
-
-# generates a code
-def generate_code():
-	key = b64encode(str(hash(datetime.now())))
-	return key
-
-# renders a group page
-@app.route('/group/<group_code>')
-def group(group_code):
-	if group_code in faux_groups:
-		return render_template('group.html', group_code=group_code, users=users, username=usernames)
-	else:
-		return render_template('404.html', value=group_code)
-
-# creates an event for specific user
+# create a new event
 @app.route('/user/<username>/create_event/', methods=['POST'])
 def create_event(username):
-	if username in faux_usernames:
-		start_time = request.form['start_time']
-		end_time = request.form['end_time']
-		event_id = generate_code()
-		faux_users[username][event_id] = [start_time, end_time]
+	# ------------
+	# NOTE: THIS NEEDS TO BE CHANGED AS THE CALENDAR UI IS INTEGRATED
+	# ------------
+	start = request.form['start']
+	end = request.form['end']
+
+	# append (start,end) to user's list of events
+	users_database[username].append((start,end))
+
+	# redirect to user page 
 	url = '/user/' + username
 	return redirect(url)
 
-# deletes an event for a specific user
-@app.route('/user/<username>/delete_event/<event_id>')
-def delete_event(username,event_id):
-	if username in faux_usernames:
-		faux_users[username].pop(event_id)
+# delete a specific event
+@app.route('/user/<username>/delete_event/<start>')
+def delete_event(username, start):
+	# find an event with a specific start date
+	events = users_database[username]
+	for index, value in enumerate(events):
+		if str(value[0]) == start:
+			# if the start date is found, delete this event
+			events.pop(index)
+
+	# redirect to user page 
 	url = '/user/' + username
 	return redirect(url)
 
-# adds a user to a unique group
-@app.route('/group/<group_code>/add_user', methods=['POST'])
-def add_user(group_code):
-	if group_code in faux_groups:
-		username = request.form['username']
-		if username in faux_usernames:
-			faux_groups[group_code].append(username)
-	url = '/group/' + group_code
-	return redirect(url)
+# ---------------------------------
+# Run Flask
+# ---------------------------------
 
 if __name__ == '__main__':
 	app.run(debug=True)
+
+
