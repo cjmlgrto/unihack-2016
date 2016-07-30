20944399628aacf15faee4b9183197f60ae7e464
CJ Melegrito <cj@mlgrto.com>
2016-07-30 22:43:34 +1000

    Import models.py

--- CJ/app.py
+++ CJ/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, redirect
-from usernames import faux_usernames, faux_users
-from groups import faux_groups
+
+from models import *
+
 from datetime import datetime
 from base64 import b64encode
 
